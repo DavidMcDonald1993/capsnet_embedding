628d0af5d52d6cc46096b52c3ee331a08c581485
David McDonald <davemcdonald@live.co.uk>
2018-07-24 09:31:57 +0100

    fix

--- src/exponential_mapping_gpu.py
+++ src/exponential_mapping_gpu.py
@@ -57,7 +57,7 @@ config.gpu_options.allow_growth = True
 config.gpu_options.per_process_gpu_memory_fraction = 0.5
 
 # config.allow_soft_placement = True
-config.log_device_placement=True
+config.log_device_placement=False
 
 config.allow_soft_placement=True
 
